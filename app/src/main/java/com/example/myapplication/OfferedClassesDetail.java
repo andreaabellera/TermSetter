c2c6cc833f4e69b7c408300262d5e706df173cc8
Andrea <abelleac@myumanitoba.ca>
2021-02-22 20:07:59 -0600

    Enabled intent hierarchy
    - Created OfferedClassesDetail activity
    - OfferedClassesDetail can only be launched from its parent activity, OfferedClassesView, which can now only be launched from OfferedClassesCategories

--- app/src/main/java/com/example/myapplication/OfferedClassesDetail.java
+++ app/src/main/java/com/example/myapplication/OfferedClassesDetail.java
@@ -4,21 +4,14 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.TextView;
 
-
-public class OfferedClassesView extends AppCompatActivity {
+public class OfferedClassesDetail extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_offered_classes_view);
-    }
-
-    public void openClassDetail(View view) {
-        Intent intent = new Intent(this, OfferedClassesDetail.class);
-        startActivity(intent);
+        setContentView(R.layout.activity_offered_classes_detail);
     }
 
 }
