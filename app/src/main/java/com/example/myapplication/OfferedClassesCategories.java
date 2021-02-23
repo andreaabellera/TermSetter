2e87e2b27c2d6af19b619c5fb68ea9fbccfa530d
Andrea <abelleac@myumanitoba.ca>
2021-02-22 19:45:06 -0600

    Created Offered Classes activity

--- app/src/main/java/com/example/myapplication/OfferedClassesCategories.java
+++ app/src/main/java/com/example/myapplication/OfferedClassesCategories.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 
@@ -13,5 +15,10 @@ public class OfferedClassesCategories extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_offered_classes_categories);
+    }
+
+    public void openClassView(View view) {
+        Intent intent = new Intent(this, OfferedClassesView.class);
+        startActivity(intent);
     }
 }
