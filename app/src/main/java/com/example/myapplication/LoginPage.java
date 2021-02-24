b31e3f06a2e78c6392149957f8bbe9c1399f70cd
Kevin Kim <hyoungkyeu@gmail.com>
2021-02-24 03:24:39 -0600

    Created kevin-login-iteration1 branch and two views for sign in and registration to create a new account

---
+++ app/src/main/java/com/example/myapplication/LoginPage.java
@@ -0,0 +1,23 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class LoginPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_page);
+    }
+
+    public void onClickLoginButton(View view){
+        Toast.makeText(this, "Login Button pressed!", Toast.LENGTH_LONG).show();
+    }
+}
