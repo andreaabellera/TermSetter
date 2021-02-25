812b5c6d72f1f20d327c9cc81d33b47654689ae8
TuanDinhLe <47037229+TuanDinhLe@users.noreply.github.com>
2021-02-25 14:06:40 -0600

    Login Logic

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -11,6 +16,16 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText eName;
+    private EditText ePassword;
+    private Button eLogin;
+
+    boolean validate;
+    String Username = "Admin";
+    String Password = "12345678";
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +40,37 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+
+
+        eName = findViewById(R.id.etName);
+        ePassword = findViewById(R.id.etPassword);
+        eLogin = findViewById(R.id.btnLogin);
+        
+        eLogin.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String inputName = eName.getText().toString();
+                String inputPassword = ePassword.getText().toString();
+
+                if (inputName.isEmpty() || inputPassword.isEmpty()) {
+                    Toast.makeText(MainActivity.this, "Too empty buddy, try again!", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    validate = validate(inputName, inputPassword);
+                    if (validate) {
+                        Toast.makeText(MainActivity.this, "Welcome home!", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+    }
+
+    private boolean validate(String name, String password) {
+        boolean result = false;
+        if (name.equals(Username) && password.equals(Password)) {
+            result = true;
+        }
+        return result;
     }
 
 }
