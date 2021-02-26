f65078d0478f61fcc64c50b6a2963a4d64946830
TuanDinhLe <47037229+TuanDinhLe@users.noreply.github.com>
2021-02-26 02:34:47 -0600

    Login Logic

--- app/src/main/java/com/example/myapplication/AccountUpdateEmail.java
+++ app/src/main/java/com/example/myapplication/AccountUpdateEmail.java
@@ -18,7 +18,7 @@ import android.widget.Toast;
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_account_change_password);
+            setContentView(R.layout.activity_account_update_email);
         }
 
         public void onClickConfirmButton(View view) {
