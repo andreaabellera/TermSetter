008e518765b7a46ac902c55259463596c4d44a0a
TuanDinhLe <47037229+TuanDinhLe@users.noreply.github.com>
2021-02-26 01:15:33 -0600

    Login Logic

--- app/src/main/java/com/example/myapplication/AccountUpdateEmail.java
+++ app/src/main/java/com/example/myapplication/AccountUpdateEmail.java
@@ -2,29 +2,55 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class AccountUpdateEmail extends AppCompatActivity {
+    public class AccountUpdateEmail extends AppCompatActivity {
+        private EditText newEmail = findViewById(R.id.update_email_input1);
+        private EditText newEmailConfirm = findViewById(R.id.update_email_input2);
+        private Button change = findViewById(R.id.btn_confirm);
+        boolean validate;
 
-    private String currEmail;
-    private String newEmail;
-    private String newEmailConfirm;
+        Intent intent = getIntent();
+        Database database = (Database)intent.getSerializableExtra("database");
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_account_update_email);
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_account_change_password);
+
+            change.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String inputNewEmail = newEmail.getText().toString();
+                    String inputNewEmailConfirm = newEmailConfirm.getText().toString();
+
+                    if (inputNewEmail.isEmpty() || inputNewEmailConfirm.isEmpty()) {
+                        Toast.makeText(AccountUpdateEmail.this, "Too empty buddy, try again!", Toast.LENGTH_SHORT).show();
+                    } else {
+                        validate = validate(inputNewEmail, inputNewEmailConfirm);
+                        if (validate) {
+                            Toast.makeText(AccountUpdateEmail.this, "Password changes!", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(AccountUpdateEmail.this, AccountManagementMenu.class);
+                            intent.putExtra("database", database);
+                            startActivity(intent);
+                        } else {
+                            Toast.makeText(AccountUpdateEmail.this, "Please try again!", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }
+            });
+        }
+
+        private boolean validate(String newEmail, String newEmailConfirm) {
+            boolean result = false;
+            if (newEmail.equals(newEmailConfirm)) {
+                result = true;
+            }
+            return result;
+        }
     }
-
-    public void updateEmail(View view)
-    {
-        // validate new email
-            // not null
-            // not = curr email
-        // validate confirm
-            // confirm = new email
-        // if both^
-            // curr email = new email
-    }
-}
