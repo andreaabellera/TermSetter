43a3f0294a567e4807111e8317930f6c44d56812
Andrea <abelleac@myumanitoba.ca>
2021-03-16 14:03:11 -0500

    OfferedClassCategories now dynamic
    - RecyclerView objects and adapter set up
    - OfferedClassCategories now generates buttons dynamically

--- app/src/main/java/comp3350/termsetter/Presentation/OfferedClassesCategories.java
+++ app/src/main/java/comp3350/termsetter/Presentation/OfferedClassesCategories.java
@@ -4,9 +4,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import comp3350.termsetter.Logic.OfferedClassLogic;
+import comp3350.termsetter.Logic.RecyclerViewDataAdapter;
 import comp3350.termsetter.Persistence.Faculty;
 import comp3350.termsetter.R;
 import java.io.InputStream;
@@ -18,54 +24,45 @@ import comp3350.termsetter.Persistence.CourseCategoryDriver;
 public class OfferedClassesCategories extends AppCompatActivity {
 
     List<Faculty> courseData;
-    int btnCount = 4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_offered_classes_categories);
-        init();
+        initDB();
+        initWidgets();
     }
 
-    public void openClassViewAcc(View view) {
-        Intent intent = new Intent(this, OfferedClassesView.class);
-        intent.putExtra("faculty", courseData.get(0));
-        startActivity(intent);
-    }
-
-    public void openClassViewCS(View view) {
-        Intent intent = new Intent(this, OfferedClassesView.class);
-        intent.putExtra("faculty", courseData.get(1));
-        startActivity(intent);
-    }
-
-    public void openClassViewMath(View view) {
-        Intent intent = new Intent(this, OfferedClassesView.class);
-        intent.putExtra("faculty", courseData.get(2));
-        startActivity(intent);
-    }
-
-    public void openClassViewMIS(View view) {
-        Intent intent = new Intent(this, OfferedClassesView.class);
-        intent.putExtra("faculty", courseData.get(3));
-        startActivity(intent);
-    }
-
-    private void init(){
+    private void initDB(){
         try{
             OfferedClassLogic dataRequester = new OfferedClassLogic(false, this);
             courseData = dataRequester.getCourseData();
-
-            for(int i = 0; i < btnCount; i++) {
-                int num = i + 1; // must be done explicitly
-                int btnID = getResources().getIdentifier("category" + num + "_btn", "id", getPackageName());
-                Button button = findViewById(btnID);
-                button.setText(courseData.get(i).getName());
-            }
         }
         catch(Exception e){
             System.out.println("Database load failed.");
             e.printStackTrace();
         }
     }
+
+    private void initWidgets(){
+        RecyclerView faculties = (RecyclerView)findViewById(R.id.faculties_rv);
+        GridLayoutManager layoutManager = new GridLayoutManager(this,2);
+        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        faculties.setLayoutManager(layoutManager);
+        RecyclerViewDataAdapter recyclerAdapter= new RecyclerViewDataAdapter(courseData);
+        faculties.setAdapter(recyclerAdapter);
+        recyclerAdapter.setOnClick(this::onItemClick);
+    }
+
+    public void onItemClick(int position) {
+        Intent intent = new Intent(this, OfferedClassesView.class);
+        intent.putExtra("faculty", courseData.get(0));
+        startActivity(intent);
+    }
+
+    public void backToMenu(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
 }
