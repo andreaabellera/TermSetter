445cdd535ff5ffdcc1e8c571ad5bbd3970d97525
Kevin Kim <hyoungkyeu@gmail.com>
2021-03-19 01:02:37 -0500

    Fixed error checking when creating an account.

--- app/src/main/java/comp3350/termsetter/Presentation/CreateAccount.java
+++ app/src/main/java/comp3350/termsetter/Presentation/CreateAccount.java
@@ -10,6 +10,9 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import comp3350.termsetter.Persistence.DomainSpecific.StubDatabase;
 import comp3350.termsetter.R;
 import comp3350.termsetter.Persistence.DomainSpecific.User;
@@ -44,31 +47,45 @@ public class CreateAccount extends AppCompatActivity {
     }
 
     public static boolean validate(String name, String id, String password, String confirmPassword, String email, String phone) {
-        if(name.isEmpty() || !(name.length() <= 30)){
-            Toast.makeText(mContext, "The length of name can be up to 30 characters.", Toast.LENGTH_SHORT).show();
+        Pattern p;
+        Matcher m;
+
+        // 1. Check Name
+        p = Pattern.compile("^[a-zA-Z]+\\s{1}[a-zA-z]+$");
+        m = p.matcher(name);
+        if(!m.matches() || name.isEmpty() || !(name.length() <= 30)){
+            Toast.makeText(mContext, "The name consist of First name, 1 whitespace, and Last name.", Toast.LENGTH_SHORT).show();
             return false;
         }
-        if(id.isEmpty() || !(id.length() <= 20)){
-            Toast.makeText(mContext, "Your student ID can be up to 20 characters.", Toast.LENGTH_SHORT).show();
+        // 2. Check ID (must start with a character, then some numbers)
+        p = Pattern.compile("^[a-zA-Z]+[0-9]*$");
+        m = p.matcher(id);
+        if(!m.matches() || id.isEmpty() || !(id.length() <= 20)){
+            Toast.makeText(mContext, "Your student ID must begin with a character.", Toast.LENGTH_SHORT).show();
             return false;
         }
+        // 3. Check password
         if(password.isEmpty()){
             Toast.makeText(mContext, "Please check password again!", Toast.LENGTH_SHORT).show();
             return false;
         }
+        // 4. Check confirm password
         if(confirmPassword.isEmpty() || !(confirmPassword.equals(password))){
             Toast.makeText(mContext, "Please check confirm password again!", Toast.LENGTH_SHORT).show();
             return false;
         }
-        if(email.isEmpty() || !(email.contains("@myumanitoba.ca")) || !(email.length() > "@myumanitoba.ca".length())){
+        // 5. Check email
+        p = Pattern.compile("^[a-zA-Z]+[0-9]*@myumanitoba\\.ca$");
+        m = p.matcher(email);
+        if(!m.matches() || email.isEmpty() || !(email.contains("@myumanitoba.ca")) || !(email.length() > "@myumanitoba.ca".length())){
             Toast.makeText(mContext, "Your email domain should contain @myumanitoba.ca and some strings in front.", Toast.LENGTH_SHORT).show();
             return false;
         }
-        if(phone.isEmpty() || !(phone.length() == 10)){
+        // 6. Check phone number (guaranteed to be just number)
+        if(phone.isEmpty() || phone.length() != 10){
             Toast.makeText(mContext, "Your phone number has to be exactly 10 numbers.", Toast.LENGTH_SHORT).show();
             return false;
         }
-
 
         return true;
     }
