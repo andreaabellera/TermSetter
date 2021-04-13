b970ccd951fd04f3732e197169eefe8051681f68
Kevin Kim <hyoungkyeu@gmail.com>
2021-04-13 13:42:33 -0500

    validation

--- app/src/main/java/comp3350/termsetter/Logic/AccountValidation.java
+++ app/src/main/java/comp3350/termsetter/Logic/AccountValidation.java
@@ -80,11 +80,7 @@ public class AccountValidation{
         // check if a student exists, and has valid ID and password
         // then check if the student record matches
         Student student = database.getStudent(sID);
-
-        if(student != null && student.getPassword().equals(password))
-                return true;
-        
-        return false;
+        return student != null && student.getPassword().equals(password);
     }
 
 }
