2b4eca80546704d2f6bb115ff539a9d3b18022b9
Andrea Abellera <andrea.abellera@gmail.com>
2021-04-15 03:14:04 -0500

    Unenrollment and database changes

--- SQL Statement Generator/scripts/generate_sql_courses.py
+++ SQL Statement Generator/scripts/generate_sql_courses.py
@@ -21,17 +21,18 @@ def generateRandomLine(section, days, timesMWF, timesTR, periods):
 
 def generateNewLines(filename):
     lines = []
+    curr_faculty = "None"
     with open(inputfilename, 'r') as file:
         for line in file:
             line = line.strip()
 
-            if line.find('$') != -1:
-                lines.append(line)
+            if line[0] == '$':
+                curr_faculty = line[1:]
             else:
                 for section in sections:
-                    extraLine = generateRandomLine(section, days, timesMWF, timesTR, periods)
-                    newLine = line + extraLine
+                    newLine = curr_faculty + "#" + line + generateRandomLine(section, days, timesMWF, timesTR, periods)
                     lines.append(newLine)
+
     return lines
 
 
@@ -44,26 +45,27 @@ def generateSQLStatements(lines, tablename):
         if len(separated_lines) == 1:
             continue
 
-        # course_id           = separated_lines[0] -> str
-        # course_name         = separated_lines[1] -> str
-        # course_credit_hours = separated_lines[2] -> int
-        # course_section      = separated_lines[3] -> str
-        # course_days         = separated_lines[4] -> str
-        # course_time         = separated_lines[5] -> hour:minute-hour:minute
-        # course_period       = separated_lines[6] -> month:day-month:day
+        # faculty_name        = separated_lines[0] -> str
+        # course_id           = separated_lines[1] -> str
+        # course_name         = separated_lines[2] -> str
+        # credit_hours        = separated_lines[3] -> int
+        # section             = separated_lines[4] -> str
+        # days                = separated_lines[5] -> str
+        # time                = separated_lines[6] -> hour:minute-hour:minute
+        # period              = separated_lines[7] -> month:day-month:day
 
-        statement = '''INSERT INTO %s (course_id, course_name, course_credit_hours, course_section, course_days, course_time, course_period) VALUES (\'%s\', \'%s\', %s, \'%s\', \'%s\', \'%s\', \'%s\');''' % (
+        statement = '''INSERT INTO %s VALUES (\'%s\', \'%s\', \'%s\', %s, \'%s\', \'%s\', \'%s\', \'%s\');''' % (
             tablename, separated_lines[0], 
             separated_lines[1], separated_lines[2], 
             separated_lines[3], separated_lines[4], 
-            separated_lines[5], separated_lines[6]
+            separated_lines[5], separated_lines[6], separated_lines[7]
         )
         statements.append(statement)
 
     return statements
 
 
-sections = ["A01"]
+sections = ["A01", "A02", "A03"]
 days = ["MWF","TR"]
 timesMWF = [
     "09:30-10:20",
