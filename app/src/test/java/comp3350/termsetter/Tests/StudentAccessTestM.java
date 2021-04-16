bc7f26dcfd6408b324a3638168a3924ea69af6f1
farjadtariq <49763743+farjadtariq@users.noreply.github.com>
2021-04-15 19:57:27 -0500

    Added StudentAccessTest

--- app/src/test/java/comp3350/termsetter/Tests/StudentAccessTestM.java
+++ app/src/test/java/comp3350/termsetter/Tests/StudentAccessTestM.java
@@ -2,80 +2,83 @@ package comp3350.termsetter.Tests;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentMatchers;
 
-import java.io.File;
-import java.io.IOException;
-import java.sql.SQLException;
 import static org.junit.Assert.*;
+
+import static org.mockito.ArgumentMatchers.same;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import comp3350.termsetter.Persistence.DomainSpecific.Student;
 import comp3350.termsetter.Persistence.DomainSpecific.hsqldbObjects.StudentAccess;
-import comp3350.termsetter.utils.TestUtils;
 
-public class StudentAccessTest {
 
-    StudentAccess sa;
-    private File tempDB;
-    Student test1;
+public class StudentAccessTestM {
+
+    private StudentAccess sa;
+    private Student test1;
 
     @Before
-    public void setup() throws IOException {
-        this.tempDB = TestUtils.copyDB();
-        sa = new StudentAccess(this.tempDB.getAbsolutePath().replace(".script", ""));
+    public void setUp(){
+        sa = mock(StudentAccess.class);
         test1 = new Student("Test Student", "yolo1234", "teststu@myumanitoba.ca", "2049058739", "testS");
     }
 
     @Test
-    public void testStudentAccessExists() throws SQLException{
+    public void testStudentAccessExists(){
         System.out.println("\nStarting testStudentAccessExists: object exists after creation\n");
         assertNotNull(sa);
         System.out.println("End testStudentAccessExists: object exists after creation\n");
     }
 
     @Test
-    public void testInsertStudent() throws SQLException {
+    public void testInsertStudent(){
         System.out.println("\nStarting testInsertStudent: object exists after creation\n");
+        when(sa.insertStudent(test1)).thenReturn(test1);
         assertNotNull(sa.insertStudent(test1));
+        verify(sa).insertStudent(test1);
         System.out.println("End testInsertStudent: object exists after creation\n");
     }
 
     @Test
-    public void testGetStudent() throws SQLException {
+    public void testGetStudent(){
         System.out.println("\nStarting testGetStudent: objects are retrieved\n");
-        sa.insertStudent(test1);
+        when(sa.getStudent(test1.getStudentID())).thenReturn(test1);
         assertNotNull(sa.getStudent(test1.getStudentID()));
+        verify(sa).getStudent(test1.getStudentID());
         System.out.println("End testGetStudent: objects are retrieved\n");
     }
 
     @Test
-    public void testCurrentStudentID() throws SQLException {
+    public void testCurrentStudentID(){
         System.out.println("\nStarting testCurrentStudentID: object exists after creation\n");
-        sa.insertStudent(test1);
+        when(sa.getCurrentStudentID()).thenReturn(test1);
         sa.setCurrentStudentID(test1.getStudentID());
         assertNotNull(sa.getCurrentStudentID());
+        verify(sa).getCurrentStudentID();
+        verify(sa).setCurrentStudentID(ArgumentMatchers.same(test1.getStudentID()));
         System.out.println("End testCurrentStudentID: object exists after creation\n");
     }
 
     @Test
-    public void testUpdatePassword() throws SQLException {
+    public void testUpdatePassword(){
         System.out.println("\nStarting testUpdatePassword: object exists after creation\n");
-        sa.insertStudent(test1);
-        sa.setCurrentStudentID(test1.getStudentID());
         String nPass = "update123";
+        when(sa.updatePassword(nPass)).thenReturn(true);
         assertTrue(sa.updatePassword(nPass));
-        assertEquals(sa.getCurrentStudentID().getPassword(), nPass);
+        verify(sa).updatePassword(nPass);
         System.out.println("End testUpdatePassword: object exists after creation\n");
     }
 
     @Test
-    public void testUpdateEmail() throws SQLException {
+    public void testUpdateEmail(){
         System.out.println("\nStarting testUpdateEmail: object exists after creation\n");
-        sa.insertStudent(test1);
-        sa.setCurrentStudentID(test1.getStudentID());
         String nEmail = "newteststu@myumanitoba.ca";
+        when(sa.updateEmail(nEmail)).thenReturn(true);
         assertTrue(sa.updateEmail(nEmail));
-        assertEquals(sa.getCurrentStudentID().getEmailAddress(), nEmail);
+        verify(sa).updateEmail(nEmail);
         System.out.println("End testUpdateEmail: object exists after creation\n");
     }
-
 }
